435f1eefa46fba18d4b6b04fb514e4c77a97765f
zel-asp <185933672+zel-asp@users.noreply.github.com>
2025-11-04 20:47:57 +0800

    configured tailwind css with postcss and vite

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+        @vite('resources/css/app.css')
         <title>{{ $title ?? 'My app' }}</title>
     </head>
 
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Instrument Sans',
+          'ui-sans-serif',
+          'system-ui',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
+        ],
+      },
+    },
+  },
+  plugins: [],
+}
