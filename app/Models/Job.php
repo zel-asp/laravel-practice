ee82c7b137966312342fd753a3ba68bacbd5f844
zel-asp <185933672+zel-asp@users.noreply.github.com>
2025-11-06 03:31:09 +0800

    migration and eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,41 +1,14 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
+    protected $table = 'job_listing';
 
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '30,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '40,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '50,000'
-            ],
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        //this will get the arra in method alL, check each index of array and return the values that satisfy the condition
-        $job = Arr::first(static::all(), fn($job) => $job['id'] === (int) $id);
-
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = [
+        'title',
+        'salary'
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Models\Job;
 
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', );
 });
 
 Route::get('/jobs', function () {
