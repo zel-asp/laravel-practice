41dc19963095cab8ecbd74304984025fdf340e62
zel-asp <185933672+zel-asp@users.noreply.github.com>
2025-11-11 02:30:29 +0800

    Enhance authentication features: add login/register templates and optimize layout components

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
         <div class="space-y-5">
             <div class="border-b border-gray-900/10 pb-12">
-                <h2 class="text-base/7 font-semibold text-gray-900">Register</h2>
+                <h2 class="text-base/7 font-semibold text-gray-900">Login</h2>
                 <p class="mt-1 text-sm/6 text-gray-600">This information will not be displayed publicly.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -29,36 +29,18 @@
                                         <x-nav-link href="/contact"
                                             :active="request()->is('contact')">Contact</x-nav-link>
                                     </div>
+
                                 </div>
                             </div>
                         </div>
-                        <div class="hidden md:block">
-                            <div class="ml-4 flex items-center md:ml-6">
-                                <button type="button"
-                                    class="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
-                                    <span class="absolute -inset-1.5"></span>
-                                    <span class="sr-only">View notifications</span>
-                                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"
-                                        data-slot="icon" aria-hidden="true" class="size-6">
-                                        <path
-                                            d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-                                            stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>
-                                </button>
-
-                                <!-- Profile dropdown -->
-                                <el-dropdown class="relative ml-3">
-                                    <button
-                                        class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-                                        <span class="absolute -inset-1.5"></span>
-                                        <span class="sr-only">Open user menu</span>
-                                        <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                            alt=""
-                                            class="size-8 rounded-full outline -outline-offset-1 outline-white/10" />
-                                    </button>
-                                </el-dropdown>
+                        @guest
+                            <div class="hidden md:block">
+                                <div class="ml-4 flex items-center md:ml-6">
+                                    <x-nav-link href="/register" :active="request()->is('/register')">Register</x-nav-link>
+                                    <x-nav-link href="/login" :active="request()->is('/login')">Login</x-nav-link>
+                                </div>
                             </div>
-                        </div>
+                        @endguest
                     </div>
                 </div>
             </nav>
